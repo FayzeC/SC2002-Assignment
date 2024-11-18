5628554a07f8830664ad1128a745dfe3ad9c3dd3
weih04 <167977880+weih04@users.noreply.github.com>
2024-11-18 15:48:05 +0800

    Update StaffCreationService.java

--- staffmanagementsystem/StaffCreationService.java
+++ staffmanagementsystem/StaffCreationService.java
@@ -4,7 +4,7 @@ import roles.Administrator;
 import roles.Doctor;
 import roles.Pharmacist;
 import roles.User;
-import org.mindrot.jbcrypt.BCrypt;
+
 import java.io.IOException;
 
 /**
@@ -18,25 +18,23 @@ public class StaffCreationService {
      * Validates the role and assigns the appropriate subclass for the staff member
      * (e.g., {@link Doctor}, {@link Pharmacist}, {@link Administrator}).
      *
-     * @param id        The hospital ID of the staff.
-     * @param role      The role of the staff (Doctor, Pharmacist, Administrator).
-     * @param name      The name of the staff.
-     * @param gender    The gender of the staff (Male/Female).
-     * @param age       The age of the staff.
-     * @param password  The password for the staff account, which will be hashed.
+     * @param id         The hospital ID of the staff.
+     * @param role       The role of the staff (Doctor, Pharmacist, Administrator).
+     * @param name       The name of the staff.
+     * @param gender     The gender of the staff (Male/Female).
+     * @param age        The age of the staff.
+     * @param password   The password for the staff account, which will be hashed.
+     * @param firstLogin
      * @return The newly created staff member as a {@link User} object.
-     * @throws IOException If there is an error during the staff creation process.
+     * @throws IOException           If there is an error during the staff creation process.
      * @throws IllegalStateException If the provided role is unexpected or invalid.
      */
-    public User createStaffMember(String id, String role, String name, String gender, String age, String password) throws IOException {
-        // Hash the password
-        String hashedPassword = BCrypt.hashpw(password, BCrypt.gensalt());
-
+    public static User createStaffMember(String id, String role, String name, String gender, String age, String password, boolean firstLogin) throws IOException {
         // Create the staff member based on the role
         return switch (role) {
-            case "Doctor" -> new Doctor(id, name, hashedPassword, role, gender, age, true);
-            case "Pharmacist" -> new Pharmacist(id, name, hashedPassword, role, gender, age, true);
-            case "Administrator" -> new Administrator(id, name, hashedPassword, role, gender, age, true);
+            case "Doctor" -> new Doctor(id, name, password, role, gender, age, firstLogin);
+            case "Pharmacist" -> new Pharmacist(id, name, password, role, gender, age, firstLogin);
+            case "Administrator" -> new Administrator(id, name, password, role, gender, age, firstLogin);
             default -> throw new IllegalStateException("Unexpected role: " + role);
         };
     }
